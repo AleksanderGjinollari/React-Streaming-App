37bd4c28985d49a7db833cb24de8c0ffaf4c2ed8
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-07-13 15:22:51 +0200

    added react router and routes to components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 // const PageOne = () => {
 //   return (
@@ -27,8 +28,9 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <Route path="/" exact component={StreamList}></Route>
         <Route path="/streams/new" exact component={StreamCreate}></Route>
         <Route path="/streams/edit" exact component={StreamEdit}></Route>
