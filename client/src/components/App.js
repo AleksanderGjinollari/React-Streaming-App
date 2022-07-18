e5b4e3693312f22b794b1fca9c0b80821216f695
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-07-18 11:44:55 +0200

    added history object and file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import Header from "./Header";
+import history from "../history";
 
 // const PageOne = () => {
 //   return (
@@ -29,14 +30,14 @@ import Header from "./Header";
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList}></Route>
         <Route path="/streams/new" exact component={StreamCreate}></Route>
         <Route path="/streams/edit" exact component={StreamEdit}></Route>
         <Route path="/streams/delete" exact component={StreamDelete}></Route>
         <Route path="/streams/show" exact component={StreamShow}></Route>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
